03f415d0b7f731eab91f69600720817ea424ec0c
Madhav Singh <madhav26266@gmail.com>
2026-04-11 11:57:53 +0530

    update index.js

--- codeclash-backend/index.js
+++ codeclash-backend/index.js
@@ -13,6 +13,7 @@ databaseConnection();
 
 app.use("/", require("./routes"));
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
